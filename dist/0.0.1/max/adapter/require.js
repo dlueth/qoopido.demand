ac4a7e34f8eb7d4b6cc72b1cfeb122f568ed2db2
Dirk Lüth <info@qoopido.com>
2015-09-03 16:46:06 +0200

    [UPDATE] build

--- dist/0.0.1/max/adapter/require.js
+++ dist/0.0.1/max/adapter/require.js
@@ -12,11 +12,11 @@
     "use strict";
     function definition(demand, provide) {
         function require() {
-            var dependencies = Array.isArray(arguments[0]) ? arguments[0] : null, callback = arguments[dependencies ? 1 : 0];
+            var parameter = arguments, dependencies = Array.isArray(parameter[0]) ? parameter[0] : null, callback = arguments[dependencies ? 1 : 0];
             demand.apply(null, dependencies || []).then(callback);
         }
         function define() {
-            var id = typeof arguments[0] === "string" ? arguments[0] : null, dependencies = Array.isArray(arguments[id ? 1 : 0]) ? arguments[id ? 1 : 0] : null, factory = arguments[id ? dependencies ? 2 : 1 : dependencies ? 1 : 0], temp = provide.apply(null, id ? [ id, factory ] : [ factory ]);
+            var parameter = arguments, id = typeof parameter[0] === "string" ? parameter[0] : null, dependencies = Array.isArray(parameter[id ? 1 : 0]) ? parameter[id ? 1 : 0] : null, factory = parameter[id ? dependencies ? 2 : 1 : dependencies ? 1 : 0], temp = provide.apply(null, id ? [ id, factory ] : [ factory ]);
             if (dependencies) {
                 temp.when.apply(null, dependencies);
             }
