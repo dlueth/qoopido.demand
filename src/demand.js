7c350186763ee48b233b29f5631071cebf786f1b
Dirk Lüth <info@qoopido.com>
2015-09-04 13:52:16 +0200

    [DEBUG] IE

--- src/demand.js
+++ src/demand.js
@@ -228,9 +228,11 @@
 					if(isAbsolute(aPath)) {
 						aPath = base.remove(resolve.url(base.url + aPath).href);
 					} else {
+						var backup = aPath;
+
 						aPath = resolve.url(((aParent && aParent.path + '/../') || '/') + aPath).pathname.replace(regexMatchAbsentSlash, '/$1');
 						var temp1 = aParent && aParent.path && resolve.url(aParent.path).href || null,
-							temp2 = resolve.url((temp1 || '/') + aPath).href;
+							temp2 = resolve.url((temp1 || '/') + backup).href;
 
 						console.log(temp1, temp2);
 						//aPath = resolve.url(((aParent && aParent.path + '/../') || '/') + aPath).href;
