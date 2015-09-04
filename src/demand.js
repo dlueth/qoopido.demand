8375dd38a78753cf2c40e29570125f7f84c62734
Dirk Lüth <info@qoopido.com>
2015-09-04 13:53:33 +0200

    [DEBUG] IE

--- src/demand.js
+++ src/demand.js
@@ -231,10 +231,10 @@
 						var backup = aPath;
 
 						aPath = resolve.url(((aParent && aParent.path + '/../') || '/') + aPath).pathname.replace(regexMatchAbsentSlash, '/$1');
-						var temp1 = aParent && aParent.path && resolve.url(aParent.path).href || null,
+						var temp1 = aParent && aParent.path && resolve.url(aParent.path + '/../').href || null,
 							temp2 = resolve.url((temp1 || '/') + backup).href;
 
-						console.log(temp1, temp2);
+						console.log(((temp1 || '/') + backup), temp1, temp2);
 						//aPath = resolve.url(((aParent && aParent.path + '/../') || '/') + aPath).href;
 					}
 
