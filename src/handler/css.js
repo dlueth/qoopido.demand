fc2402ad1c75c2dc6d4da1812c8c3e8cfd4bc62d
Dirk Lüth <info@qoopido.com>
2015-09-27 12:26:09 +0200

    [BUGFIX] handler/css
    - fix bugs regarding path to URL conversion

--- src/handler/css.js
+++ src/handler/css.js
@@ -13,10 +13,21 @@
 ;(function(document, setTimeout) {
 	'use strict';
 
-	var target           = document.getElementsByTagName('head')[0],
-		regexMatchCssUrl = /url\(\s*(?:"|'|)(?!data:|http:|https:|\/)(.+?)(?:"|'|)\)/g;
+	var target              = document.getElementsByTagName('head')[0],
+		resolver            = document.createElement('a'),
+		regexMatchUrl       = /url\s*\(\s*["']?(.+?)["']?\s*\)/gi,
+		regexMatchImport    = /@import\s+["'](.+?)["']/gi,
+		regexMatchProtocol  = /http(s?):\/\//gi,
+		regexIsAbsolutePath = /^\//i,
+		regexIsAbsoluteUri  = /^data:|http(s?):|\/\//i;
 
-	function definition(resolveUrl) {
+	function resolveUrl(url) {
+		resolver.href = url;
+
+		return resolver;
+	}
+
+	function definition() {
 		return {
 			matchType: /^text\/css/,
 			onPreRequest: function() {
@@ -26,14 +37,26 @@
 				self.url = url.slice(-4) !== '.css' ? url + '.css' : url;
 			},
 			onPostRequest: function() {
-				var self   = this,
-					base   = resolveUrl(self.url + '/..'),
-					source = self.source,
+				var self    = this,
+					url     = resolveUrl(self.url + '/..'),
+					base    = url.href,
+					host    = '//' + url.host,
+					source  = self.source,
 					match;
 
-				while((match = regexMatchCssUrl.exec(source))) {
-					source = source.replace(match[0], 'url(' + resolveUrl(base + match[1]) + ')');
+				while((match = regexMatchUrl.exec(source))) {
+					if(!regexIsAbsoluteUri.test(match[1])) {
+						source = source.replace(match[0], 'url("' + resolveUrl(regexIsAbsolutePath.test(match[1]) ? host + match[1] : base + match[1]).href + '")');
+					}
 				}
+
+				while((match = regexMatchImport.exec(source))) {
+					if(!regexIsAbsoluteUri.test(match[1])) {
+						source = source.replace(match[0], '@import "' + resolveUrl(regexIsAbsolutePath.test(match[1]) ? host + match[1] : base + match[1]).href + '"');
+					}
+				}
+
+				source = source.replace(regexMatchProtocol, '//');
 
 				self.source = source;
 			},
@@ -61,5 +84,5 @@
 		};
 	}
 
-	provide([ '/demand/function/resolveUrl' ], definition);
+	provide(definition);
 }(document, setTimeout));
