f91e2ae98969102a9df157c081e3659ecb672ddc
Dirk Lüth <info@qoopido.com>
2015-09-27 20:31:34 +0200

    [UPDATE] handler/text
    - add handler/text

---
+++ src/handler/text.js
@@ -0,0 +1,27 @@
+/**
+ * Qoopido.demand handler/text
+ *
+ * Copyright (c) 2015 Dirk Lueth
+ *
+ * Dual licensed under the MIT and GPL licenses.
+ *  - http://www.opensource.org/licenses/mit-license.php
+ *  - http://www.gnu.org/copyleft/gpl.html
+ *
+ * @author Dirk Lueth <info@qoopido.com>
+ */
+
+;(function() {
+	'use strict';
+
+	function definition() {
+		return {
+			process: function() {
+				var self = this;
+
+				provide(function() { return self.source; });
+			}
+		};
+	}
+
+	provide(definition);
+}());
