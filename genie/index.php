4d3a66d45c8f6903b5467053a15fcfa33e240ae4
Dirk Lüth <dlueth@Dirks-MacBook-Pro.local>
2017-01-25 11:18:40 +0100

    add file caching to basic genie implementation

--- genie/index.php
+++ genie/index.php
@@ -1,23 +1,38 @@
 <?php
 define('BASE', '../demo/');
+define('CACHE_DIRECTORY', './temp/');
 
 header('Content-Type: application/javascript; charset=utf-8');
 
-ob_start();
-
 if(isset($_GET['module']) && is_array($_GET['module'])) {
-    foreach($_GET['module'] as $module) {
-        if(!@readfile(BASE . $module . '.js')) {
-            ob_end_clean();
-            header('HTTP/1.0 404 Not Found');
-            die();
+    $file = CACHE_DIRECTORY . sha1(json_encode($_GET['module'])) . '.js';
+
+    if(file_exists($file)) {
+        echo '/* from cache */';
+        @readfile($file);
+    } else {
+        if(!is_dir(CACHE_DIRECTORY)) {
+            mkdir(CACHE_DIRECTORY, 0770, true);
         }
 
-        echo chr(10);
+        ob_start();
+
+        foreach($_GET['module'] as $module) {
+            if(!@readfile(BASE . $module . '.js')) {
+                ob_end_clean();
+                header('HTTP/1.0 404 Not Found');
+                die();
+            }
+
+            echo chr(10);
+        }
+
+        $content = ob_get_contents();
+        ob_end_flush();
+
+        @file_put_contents($file, $content, LOCK_EX);
     }
 }
-
-ob_end_flush();
 
 die();
 ?>
