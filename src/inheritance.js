c5f8125ee2f4a3a35ff86d5cedf032b402c57c0c
Dirk Lüth <dlueth@Dirks-MBP.fritz.box>
2018-02-05 22:57:43 +0100

    adjust inheritance

--- src/inheritance.js
+++ src/inheritance.js
@@ -1,34 +1,40 @@
 /* global
 	global, document, demand, provide, queue, processor, settings, setTimeout, clearTimeout, storage,
+	TRUE,
 	objectCreate, objectDefineProperty, objectGetOwnPropertyNames, objectGetOwnPropertyDescriptor,
 	ClassDescriptor
 */
 
+//=require constants.js
 //=require shortcuts.js
 //=require class/descriptor.js
 
-(function() {
-	var descriptor;
-
-	function extend(parent) {
+(function(strPrototype) {
+	function functionExtends(source) {
 		var self       = this,
-			prototype  = self.prototype,
-			properties = {},
+			prototype  = self[strPrototype],
 			names      = objectGetOwnPropertyNames(prototype),
+			properties = { constructor:  new ClassDescriptor(self, TRUE, TRUE)},
 			i = 0, property;
 
-		for(; (property = names[i]); i++) {
+		for(; (property = names[i]) && !properties[property]; i++) {
 			properties[property] = objectGetOwnPropertyDescriptor(prototype, property);
 		}
 
-		properties.constructor = new ClassDescriptor(self);
-		self.prototype         = objectCreate(parent.prototype || parent, properties);
+		try {
+			self[strPrototype] = objectCreate(source[strPrototype] || source, properties);
+		} catch(e) {
+			// ES6 class prototypes are readonly at least in Chrome
+			// (and assignment throws an error in strict mode) ...
+		}
+
+		// ... which is handled here
+		if(self[strPrototype] === prototype) {
+			throw new TypeError('Unable to extend, prototype is not writable');
+		}
 
 		return self;
 	}
 
-	descriptor = new ClassDescriptor(extend);
-
-	objectDefineProperty(Function.prototype, 'extends', descriptor);
-	objectDefineProperty(global.Function.prototype, 'extends', descriptor);
-}());
+	objectDefineProperty(Function[strPrototype], 'extends', new ClassDescriptor(functionExtends));
+}('prototype'));
