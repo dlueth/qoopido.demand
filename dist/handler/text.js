cc62d2c8b73727098d1157bc991320195f4d690f
Dirk Lüth <dlueth@Dirks-MBP.fritz.box>
2018-02-05 22:57:50 +0100

    build 4.2.7-alpha.1

--- dist/handler/text.js
+++ dist/handler/text.js
@@ -1,3 +1,3 @@
-/**! Qoopido.demand 4.2.6 | https://github.com/dlueth/qoopido.demand | (c) 2018 Dirk Lueth */
+/**! Qoopido.demand 4.2.7-alpha.1 | https://github.com/dlueth/qoopido.demand | (c) 2018 Dirk Lueth */
 !function(){"use strict";provide(["/demand/abstract/handler"],function(t){var n=/^text\/.+/;function e(){}return e.prototype={validate:function(t){return n.test(t)},process:function(t){provide(function(){return t.source})}},new(e.extends(t))})}();
 //# sourceMappingURL=text.js.map
