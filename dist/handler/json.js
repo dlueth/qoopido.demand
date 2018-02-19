43fe862caf0a97a7749222fa620670747d53a177
Dirk Lüth <dlueth@Dirks-MacBook-Pro.local>
2018-02-19 15:46:42 +0100

    bump & build

--- dist/handler/json.js
+++ dist/handler/json.js
@@ -1,3 +1,3 @@
-/**! Qoopido.demand 5.0.0 | https://github.com/dlueth/qoopido.demand | (c) 2018 Dirk Lueth */
-!function(){"use strict";provide(["/demand/abstract/handler"],function(n){var t=/^application\/json/;function e(){}return e.prototype={validate:function(n){return t.test(n)},onPreRequest:function(n){var t=n.url.pathname;n.url.pathname=".json"!==t.slice(-".json".length)?t+".json":t},process:function(n){var t=JSON.parse(n.source);provide(function(){return t})}},new(e.extends(n))})}();
+/**! Qoopido.demand 5.0.1 | https://github.com/dlueth/qoopido.demand | (c) 2018 Dirk Lueth */
+!function(){"use strict";provide(["/demand/abstract/handler"],function(n){function t(){}var e=".json",r=/^application\/json/;return t.prototype={validate:function(n){return r.test(n)},onPreRequest:function(n){var t=n.url.pathname;n.url.pathname=t.slice(-e.length)!==e?t+e:t},process:function(n){var t=JSON.parse(n.source);provide(function(){return t})}},new(t.extends(n))})}();
 //# sourceMappingURL=json.js.map
--- dist/handler/text.js
+++ dist/handler/text.js
@@ -1,3 +1,3 @@
-/**! Qoopido.demand 5.0.0 | https://github.com/dlueth/qoopido.demand | (c) 2018 Dirk Lueth */
-!function(){"use strict";provide(["/demand/abstract/handler"],function(t){var n=/^text\/.+/;function e(){}return e.prototype={validate:function(t){return n.test(t)},process:function(t){provide(function(){return t.source})}},new(e.extends(t))})}();
+/**! Qoopido.demand 5.0.1 | https://github.com/dlueth/qoopido.demand | (c) 2018 Dirk Lueth */
+!function(){"use strict";provide(["/demand/abstract/handler"],function(t){function n(){}var e=/^text\/.+/;return n.prototype={validate:function(t){return e.test(t)},process:function(t){provide(function(){return t.source})}},new(n.extends(t))})}();
 //# sourceMappingURL=text.js.map
