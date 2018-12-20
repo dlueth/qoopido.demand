b57810d2efde18728e1a42771dfa5333b88355f2
Dirk Lüth <dirk.lueth@gmail.com>
2018-12-20 09:40:07 +0100

    build release

--- dist/cache/dispose.js
+++ dist/cache/dispose.js
@@ -1,3 +1,3 @@
-/**! Qoopido.demand 5.1.1 | https://github.com/dlueth/qoopido.demand | (c) 2018 Dirk Lueth */
+/**! Qoopido.demand 5.2.0 | https://github.com/dlueth/qoopido.demand | (c) 2018 Dirk Lueth */
 !function(){"use strict";provide(["/demand/function/iterate","./states"],function(e,n){function s(e,c){return e.accessed<c.accessed?-1:e.accessed>c.accessed?1:0}return function(e){var c,t=n();for(t.sort(s);0<e&&t.length;)e-=(c=t.shift()).size,demand.cache.clear(c.id)}})}();
 //# sourceMappingURL=dispose.js.map
