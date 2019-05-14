78ea7a0d906991177d004f4806e0ebd36d6f0494
Dirk Lüth <dirk.lueth@gmail.com>
2019-05-14 22:16:25 +0200

    fix bug in weakmap regardnig IE11

--- src/class/weakmap.js
+++ src/class/weakmap.js
@@ -5,53 +5,71 @@
 
 //=require function/uuid.js
 
-var ClassWeakmap = 'WeakMap' in global ? global.WeakMap : (function() {
-	var prefix = 'weakmap-';
-
-	function getEntry(context, key) {
-		var entry;
-
-		if((entry = key[context.id]) && entry[0] === key) {
-			return entry;
+var ClassWeakmap = 'WeakMap' in global ? (function() {
+		if(((new global.WeakMap()).set(function() {}, false) instanceof global.WeakMap)) {
+			return global.WeakMap;
 		}
-	}
 
-	function ClassWeakmap() {
-		this.define('id', prefix + functionUuid());
-	}
+		return function WeakMapFill() {
+			var weakmap = new global.WeakMap();
 
-	ClassWeakmap.prototype = {
-		set: function(key, value) {
-			var entry = getEntry(this, key);
+			weakmap.set = function set() {
+				global.WeakMap.prototype.set.apply(this, arguments);
 
-			if(entry) {
-				entry[1] = value;
-			} else {
-				key.define(this.id, [ key, value ]);
+				return this;
+			};
+
+			return weakmap;
+		};
+	}())
+	:
+	(function() {
+		var prefix = 'weakmap-';
+
+		function getEntry(context, key) {
+			var entry;
+
+			if((entry = key[context.id]) && entry[0] === key) {
+				return entry;
 			}
-
-			return this;
-		},
-		get: function(key) {
-			var entry = getEntry(this, key);
-
-			if(entry) {
-				return entry[1];
-			}
-		},
-		delete: function(key) {
-			var entry = getEntry(this, key);
-
-			if(entry) {
-				entry.length = 0;
-
-				delete key[this.id];
-			}
-		},
-		has: function(key) {
-			return !!getEntry(this, key);
 		}
-	};
 
-	return ClassWeakmap;
+		function ClassWeakmap() {
+			this.define('id', prefix + functionUuid());
+		}
+
+		ClassWeakmap.prototype = {
+			set: function(key, value) {
+				var entry = getEntry(this, key);
+
+				if(entry) {
+					entry[1] = value;
+				} else {
+					key.define(this.id, [ key, value ]);
+				}
+
+				return this;
+			},
+			get: function(key) {
+				var entry = getEntry(this, key);
+
+				if(entry) {
+					return entry[1];
+				}
+			},
+			delete: function(key) {
+				var entry = getEntry(this, key);
+
+				if(entry) {
+					entry.length = 0;
+
+					delete key[this.id];
+				}
+			},
+			has: function(key) {
+				return !!getEntry(this, key);
+			}
+		};
+
+		return ClassWeakmap;
 }());
